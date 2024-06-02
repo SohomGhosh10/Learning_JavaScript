0c84e4da50fe000a4cfbb581842cef39d2adbd23
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-06-02 19:10:01 +0530

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,17 @@
+alert("Welcome to JS");
+
+console.log("I am Sohom")
+console.log("Learning Js")
+
+var a = prompt("Enter a number")
+
+console.log("Your number is " + a)
+
+var isTrue = confirm("Do you want to leave this site?")
+
+if(isTrue){
+    console.log("You successfully left this site")
+}
+else{
+    console.log("You are still there")
+}
