af97ee9c4fc308ca7722696d8172b092c99bbbaf
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-06-04 13:20:29 +0530

    Update Intro.js

--- Intro.js
+++ Intro.js
@@ -15,3 +15,18 @@ if(isTrue){
 else{
     console.log("You are still there")
 }
+var age = 10;
+console.log(age) // using var we can redeclare and update the values of variable
+/*
+var age = 20
+var age = 30*/
+
+let d = 100; // using let we can declare the variable only once but can be updated
+
+console.log(d);
+let c;
+console.log(c); // ----> it will print undefined as no value is initialized
+
+const b = 10; // It is a constant value for the particular variable assigned
+// const variable must be initialized
+console.log(b);
